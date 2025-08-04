39ab5dafc596eefef9078345f51d02e74e6f1470
Manoj Gaihre <manojgaihre7@gmail.com>
2025-08-04 08:20:31 +0545

    add user registration

--- Models/User.cs
+++ Models/User.cs
@@ -1,9 +1,17 @@
-﻿namespace OnlineShopping.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopping.Models
 {
     public class User
     {
         public int Id { get; set; }
+        //email validation
+        [DataType(DataType.EmailAddress, ErrorMessage ="Please enter valid email!!")]
         public string UserName { get; set; }
+        //password .. ko form ma dekahuna ko laghi
+        [DataType(DataType.Password)]
+        //password validation ko laghi
+        [StringLength(50,MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
         public string Password { get; set; }
         public string UserType { get; set; }
 
